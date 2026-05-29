8fed37b5cd8f3229961381b094ccbddb2fe9f96f
W501288410 <501288410@QQ.COM>
2026-05-29 18:15:40 +0800

    Add src\middleware\logger.ts

---
+++ src/middleware/logger.ts
@@ -0,0 +1,16 @@
+﻿import { type Context, type Next } from 'koa'
+import { logger } from '../utils/logger'
+
+export function requestLogger() {
+  return async (ctx: Context, next: Next) => {
+    const start = Date.now()
+    await next()
+    const ms = Date.now() - start
+    const message = `${ctx.method} ${ctx.url} ${ctx.status} ${ms}ms`
+    if (ctx.status >= 400) {
+      logger.warn(message)
+    } else {
+      logger.info(message)
+    }
+  }
+}
